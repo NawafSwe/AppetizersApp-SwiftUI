ddbb78b0b3108776fe4fbaf08d3d81186f94b40c
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-16 15:39:59 +0300

    Initial Commit

---
+++ Appetizers-App/Appetizers_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Appetizers_AppApp.swift
+//  Appetizers-App
+//
+//  Created by Nawaf B Al sharqi on 16/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Appetizers_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appetizers-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Appetizers-App
+//
+//  Created by Nawaf B Al sharqi on 16/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
